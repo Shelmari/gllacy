5296dfa5ae5fdb5ef9c6b461ebf366cecf83b6c9
Maria Koshelenko <k.mary.srg@gmail.com>
2018-08-29 17:49:43 +0300

    Added close 'ESC'

--- js/main.js
+++ js/main.js
@@ -6,19 +6,35 @@ var inputEmail = document.querySelector(".feedback__input[type=email]");
 var inputTextarea = document.querySelector(".feedback__textarea");
 var feedbackForm = document.querySelector(".feedback__form");
 
+var isStorageSupport = false;
+var storage = "";
+
+try{
+  var storage = localStorage.getItem("inputLogin");
+} catch(err) {
+  isStorageSupport = false;
+}
+
 btnMap.addEventListener("click", function(evt){
   evt.preventDefault();
   feedback.classList.add("modal__feedback_show");
-  inputLogin.focus();
+  if (storage) {
+    inputLogin.value = storage;
+    inputEmail.focus();
+  } else {
+    inputLogin.focus();
+  }
 });
 
 feedbackForm.addEventListener("submit", function(evt){
   if (!inputLogin.value || !inputEmail.value || !inputTextarea.value){
     evt.preventDefault();
     console.log("нужно ввести имя и почту с комментарием");
-  } else{
-    localStorage.setItem("inputLogin", inputLogin.value);
-    console.log("Отправил");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("inputLogin", inputLogin.value);
+      console.log("Отправил");
+    }
   }
 });
 
@@ -26,3 +42,12 @@ close.addEventListener("click", function(evt){
   evt.preventDefault();
   feedback.classList.remove("modal__feedback_show");
 });
+
+window.addEventListener("keydown", function(evt) {
+  if(evt.keyCode === 27){
+    if(feedback.classList.contains("modal__feedback_show")){
+      evt.preventDefault();
+      feedback.classList.remove("modal__feedback_show");
+    }
+  }  
+})
